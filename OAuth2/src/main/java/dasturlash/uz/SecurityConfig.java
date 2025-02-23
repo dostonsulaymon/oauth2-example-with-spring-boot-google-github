ea152598f987b334972e227a4fb764e4a8bcb9a1
Doston Sulaymon <dostonsulaymon@Dostons-MacBook-Pro.local>
2025-02-23 13:49:40 +0500

    Initial backend commit

---
+++ OAuth2/src/main/java/dasturlash/uz/SecurityConfig.java
@@ -0,0 +1,37 @@
+package dasturlash.uz;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.swing.*;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated())
+//                .formLogin(form -> form.defaultSuccessUrl("/hello", true))
+
+                .oauth2Login(
+                        oauth2Login ->
+                                oauth2Login.defaultSuccessUrl("/hello", true));
+
+        return http.build();
+
+    }
+
+}
